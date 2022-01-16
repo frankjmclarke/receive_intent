bf6b3dd2219587d1eaebf0ff2e15d551c350e3c4
unknown <ritsapptech@gmail.com>
2022-01-16 18:38:10 +0530

    Flutter code for receiving  text & files

---
+++ README.md
@@ -0,0 +1,15 @@
+# Receive sharing intent example
+
+This flutter project shows how to use receive_sharing_intent package to receive text & files.
+
+## Getting Started
+
+This project is a showcase for receiving text & files when application is opened & closed. IT shows how to use this package's different methods.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Receive_sharing_intent Package](https://pub.dev/packages/receive_sharing_intent)
+
+For help getting started with Flutter, view our
+[online documentation](https://flutter.dev/docs), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
---
+++ lib/main.dart
@@ -0,0 +1,117 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:receive_sharing_intent/receive_sharing_intent.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  StreamSubscription? _dataStreamSubscription;
+
+  String _sharedText = "";
+
+  List<SharedMediaFile>? _sharedFiles;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("Receive intent example"),
+      ),
+      body: Container(
+        margin: const EdgeInsets.only(top: 50, left: 10, right: 10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              "Shared Text is :",
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              width: 5,
+            ),
+            Text(_sharedText, style: const TextStyle(fontSize: 20)),
+            const SizedBox(height: 100),
+            const Text("Shared files:",
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
+            const SizedBox(
+              width: 5,
+            ),
+            if (_sharedFiles != null)
+              Text(_sharedFiles!.map((f) => f.path).join(",") ?? "",
+                  style: const TextStyle(fontSize: 20)),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    //Receive text data when app is running
+    _dataStreamSubscription =
+        ReceiveSharingIntent.getTextStream().listen((String text) {
+      setState(() {
+        _sharedText = text;
+      });
+    });
+
+    //Receive text data when app is closed
+    ReceiveSharingIntent.getInitialText().then((String? text) {
+      if (text != null) {
+        setState(() {
+          _sharedText = text;
+        });
+      }
+    });
+
+    //Receive files when app is running
+    _dataStreamSubscription = ReceiveSharingIntent.getMediaStream()
+        .listen((List<SharedMediaFile> files) {
+      setState(() {
+        _sharedFiles = files;
+      });
+    });
+
+    //Receive files when app is closed
+    ReceiveSharingIntent.getInitialMedia().then((List<SharedMediaFile> files) {
+      if (files != null) {
+        setState(() {
+          _sharedFiles = files;
+        });
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _dataStreamSubscription!.cancel();
+  }
+}
